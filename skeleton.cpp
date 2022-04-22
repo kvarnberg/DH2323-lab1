a9b076d54a362dabe8e4352a22ae1f65b3d85341
Josephine Kvarnberg <jkv@kth.se>
2022-04-22 11:50:20 +0200

    updated

--- skeleton.cpp
+++ skeleton.cpp
@@ -13,8 +13,6 @@
 using namespace std;
 using glm::vec3;
 
-#define TASK stars
-
 // --------------------------------------------------------
 // GLOBAL VARIABLES
 
@@ -28,7 +26,7 @@ vec3 green = vec3(0, 1, 0);
 vec3 blue = vec3(0, 0, 1);
 vec3 yellow = vec3(1, 1, 0);
 
-vector<vec3> stars(1000); // i think it looks nicer with 5000
+vector<vec3> stars(1000); // i think it looks nicer with 5000 tbh, more like star wars
 
 int t;
 
@@ -52,6 +50,7 @@ int main(int argc, char *argv[])
 {
   sdlAux = new SDL2Aux(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+  // the part commented out is for testing the functions
   /*
   vector<float> result(10);        // Create a vector with 10 floats
   InterpolateFloat(5, 14, result); // Fill it with interpolated values
@@ -94,27 +93,30 @@ int main(int argc, char *argv[])
 
 void DrawColor()
 {
-
+  // bilinear interpolation
   sdlAux->clearPixels();
 
   vector<vec3> leftYDir(SCREEN_HEIGHT);
   vector<vec3> rightYDir(SCREEN_HEIGHT);
 
-  // left side
+  // interpolate on left side, in y direction up and down, by sending in color vectors for red and yellow, as well
+  // as an empty vector of the screens height to fill with interpolated values.
   Interpolate(red, yellow, leftYDir);
-  // right side
+  // right side, in y direction, same as described before but with blue and green and done on the right side of the screen.
   Interpolate(blue, green, rightYDir);
 
+  // loop through all pixels
   for (int y = 0; y < SCREEN_HEIGHT; ++y)
   {
     for (int x = 0; x < SCREEN_WIDTH; ++x)
     {
+      // interpolate between the left side and the right side, send in empty vector for the middle which will be filled
+      // with the interpolated values. my thought process for this is described in notes, please refer to it for a great
+      // visual representation of bilinear interpolation.
+
       vector<vec3> middleLine(SCREEN_WIDTH);
       Interpolate(leftYDir[y], rightYDir[y], middleLine);
       sdlAux->putPixel(x, y, middleLine[x]);
-
-      /*vec3 color(1.0, 0.0, 1.0);
-      sdlAux->putPixel(x, y, color);*/
     }
   }
 
@@ -125,17 +127,21 @@ void DrawStars()
 {
   sdlAux->clearPixels();
 
+  // focal length
   float f = SCREEN_HEIGHT / 2;
 
+  // make screen black
   SDL_FillRect(screen, 0, 0);
+
   for (size_t s = 0; s < stars.size(); ++s)
   {
-    // equation 3
+    // equation 3, pinhole camera, transform from (x,y,z) to (u,v)
     float u = (f * (stars[s].x / stars[s].z)) + SCREEN_WIDTH / 2.0f;
     float v = (f * (stars[s].y / stars[s].z)) + SCREEN_HEIGHT / 2.0f;
 
+    // fade the brightness of the stars, i.e. let it depend on the distance to the camera, from instructions
     vec3 color = 0.2f * vec3(1, 1, 1) / (stars[s].z * stars[s].z);
-    // PutPixelSDL(screen, u, v, color);
+
     sdlAux->putPixel(u, v, color);
   }
 
@@ -151,7 +157,6 @@ void Update()
 
   for (int s = 0; s < stars.size(); ++s)
   {
-    // Add code for update of stars
     stars[s].z = stars[s].z - (v * dt); // equation 1
 
     if (stars[s].z <= 0)
@@ -174,6 +179,7 @@ void InterpolateFloat(float a, float b, vector<float> &result)
   }
   for (int i = 0; i < result.size(); ++i)
   {
+    // t ∈ [0, 1]
     float t = i / ((float)result.size() - 1.0f);
     result[i] = ((1 - t) * a) + b * t;
   }
@@ -189,6 +195,7 @@ void Interpolate(vec3 a, vec3 b, vector<vec3> &result)
   for (int i = 0; i < result.size(); i++)
   {
     //(1-t)a + t*b, for each of the three pixel values
+    // t ∈ [0, 1]
     float t = float(i) / (float(result.size()) - 1.0f);
     result[i].x = (1 - t) * a.x + t * b.x;
     result[i].y = (1 - t) * a.y + t * b.y;
