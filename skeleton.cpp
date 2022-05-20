c490ca8a3aa60687ecd09467e389ff7cf77701d2
Josephine Kvarnberg <jkv@kth.se>
2022-05-20 17:10:58 +0200

    final upload

--- skeleton.cpp
+++ skeleton.cpp
@@ -9,39 +9,57 @@
 #include </usr/local/include/glm/glm.hpp>
 #include <vector>
 #include "SDL2Auxiliary.h"
+#include "TestModel.h"
 
 using namespace std;
+using glm::mat3;
 using glm::vec3;
 
 // --------------------------------------------------------
-// GLOBAL VARIABLES
+// GLOBAL VARIABLES FROM LAB2
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
 SDL2Aux *sdlAux;
 SDL_Surface *screen;
-
-vec3 red = vec3(1, 0, 0);
-vec3 green = vec3(0, 1, 0);
-vec3 blue = vec3(0, 0, 1);
-vec3 yellow = vec3(1, 1, 0);
-
-vector<vec3> stars(1000); // i think it looks nicer with 5000 tbh, more like star wars
-
 int t;
 
+vector<Triangle> triangles;
+
+struct Intersection
+{
+  vec3 position;
+  float distance;
+  int triangleIndex;
+};
+
+float focalLength = SCREEN_HEIGHT / 2;
+vec3 cameraPos(0, 0, -2);
+
+mat3 R = mat3(vec3(1, 0, 0), vec3(0, 1, 0), vec3(0, 0, 1)); // rotation matrix for camera
+float yaw;                                                  // stored angle for camera to rotate around y axis
+float pi = 3.14159265359;
+
+vec3 lightPos(0, -0.5, -0.7);
+vec3 lightColor = 2.f * vec3(1, 1, 1);
+
 // --------------------------------------------------------
-// FUNCTION DECLARATIONS
+// FUNCTION DECLARATIONS FROM LAB2
 
-void DrawColor();
-
-void DrawStars();
-
-void InterpolateFloat(float a, float b, vector<float> &result);
-
-void Interpolate(vec3 a, vec3 b, vector<vec3> &result);
-
+void Draw();
 void Update();
+bool ClosestIntersection(
+    vec3 start,
+    vec3 dir,
+    const vector<Triangle> &triangles,
+    Intersection &closestIntersection);
+void Rotate();
+// vec3 DirectLight(const Intersection &i);
+
+/*ADDED FUNCTIONS*/
+vec3 Ambient(const Intersection &i);
+vec3 Diffuse(const Intersection &i);
+vec3 Specular(const Intersection &i);
 
 // --------------------------------------------------------
 // FUNCTION DEFINITIONS
@@ -49,156 +67,259 @@ void Update();
 int main(int argc, char *argv[])
 {
   sdlAux = new SDL2Aux(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-  // the part commented out is for testing the functions
-  /*
-  vector<float> result(10);        // Create a vector with 10 floats
-  InterpolateFloat(5, 14, result); // Fill it with interpolated values
-  for (int i = 0; i < result.size(); ++i)
-    cout << result[i] << " "; // Print the result to the terminal
-  */
-  /*
-  vector<vec3> result(4);
-  vec3 a(1, 4, 9.2);
-  vec3 b(4, 1, 9.8);
-  Interpolate(a, b, result);
-  for (int i = 0; i < result.size(); ++i)
-  {
-    cout << "( "
-         << result[i].x << ", "
-         << result[i].y << ", "
-         << result[i].z << " ) ";
-  }
-  */
-
-  // loop through all stars
-  for (int i = 0; i < stars.size(); i++)
-  {
-    // equation 2
-    stars[i].x = 2 * float(rand()) / float(RAND_MAX) - 1.0f; // between -1 and 1
-    stars[i].y = 2 * float(rand()) / float(RAND_MAX) - 1.0f; // between -1 and 1
-    stars[i].z = float(rand()) / float(RAND_MAX);            // between 0 and 1
-  }
+  t = SDL_GetTicks(); // Set start value for timer.
+  LoadTestModel(triangles);
 
   while (!sdlAux->quitEvent())
   {
     Update();
-    DrawStars();
-    // DrawColor();
+    Draw();
   }
 
-  sdlAux->saveBMP("screenshot.bmp");
+  // sdlAux->saveBMP("screenshot.bmp");
   return 0;
 }
 
-void DrawColor()
+/*FROM LAB2, BUT WITH ADDED COMPONENTS OF PHONG REFLECTION*/
+void Draw()
 {
-  // bilinear interpolation
+  // loop through all pixels and compute the corresponding ray direction.
+  // Then call the function ClosestIntersection to get the closest intersection in that direction.
+  // If there was an intersection the color of the pixel should be set to the color of that triangle.
+  // Otherwise it should be black.
   sdlAux->clearPixels();
 
-  vector<vec3> leftYDir(SCREEN_HEIGHT);
-  vector<vec3> rightYDir(SCREEN_HEIGHT);
-
-  // interpolate on left side, in y direction up and down, by sending in color vectors for red and yellow, as well
-  // as an empty vector of the screens height to fill with interpolated values.
-  Interpolate(red, yellow, leftYDir);
-  // right side, in y direction, same as described before but with blue and green and done on the right side of the screen.
-  Interpolate(blue, green, rightYDir);
-
-  // loop through all pixels
   for (int y = 0; y < SCREEN_HEIGHT; ++y)
   {
     for (int x = 0; x < SCREEN_WIDTH; ++x)
     {
-      // interpolate between the left side and the right side, send in empty vector for the middle which will be filled
-      // with the interpolated values. my thought process for this is described in notes, please refer to it for a great
-      // visual representation of bilinear interpolation.
-
-      vector<vec3> middleLine(SCREEN_WIDTH);
-      Interpolate(leftYDir[y], rightYDir[y], middleLine);
-      sdlAux->putPixel(x, y, middleLine[x]);
+      // vector d pointing in the direction where the light reaching pixel (x,y) comes from
+      vec3 direction((x - SCREEN_WIDTH / 2), (y - SCREEN_HEIGHT / 2), focalLength);
+      Intersection intersection;
+      direction = R * glm::normalize(direction); // update direction of camera with rotation matrix, so that rays go from the right place
+      if (ClosestIntersection(cameraPos, direction, triangles, intersection))
+      {
+        /*vec3 light = DirectLight(intersection);
+        vec3 colornlight = color * (light + indirectLight);*/
+        vec3 color = triangles[intersection.triangleIndex].color;
+        vec3 ambient = Ambient(intersection);
+        vec3 diffuse = Diffuse(intersection);
+        vec3 specular = Specular(intersection);
+        vec3 illumination = color * (ambient + diffuse + specular);
+        sdlAux->putPixel(x, y, illumination);
+      }
+      else
+      {
+        sdlAux->putPixel(x, y, vec3(0, 0, 0));
+      }
     }
   }
 
   sdlAux->render();
 }
 
-void DrawStars()
+/*FROM LAB2*/
+bool ClosestIntersection(vec3 start,
+                         vec3 dir,
+                         const vector<Triangle> &triangles,
+                         Intersection &closestIntersection)
 {
-  sdlAux->clearPixels();
+  // If an intersection occurred it should return true. Otherwise false.
+  // In the case of an intersection it should also return some information about the closest intersection.
 
-  // focal length
-  float f = SCREEN_HEIGHT / 2;
+  float m = std::numeric_limits<float>::max();
+  bool intersect = false;
 
-  // make screen black
-  SDL_FillRect(screen, 0, 0);
-
-  for (size_t s = 0; s < stars.size(); ++s)
+  for (int i = 0; i < triangles.size(); i++)
   {
-    // equation 3, pinhole camera, transform from (x,y,z) to (u,v)
-    float u = (f * (stars[s].x / stars[s].z)) + SCREEN_WIDTH / 2.0f;
-    float v = (f * (stars[s].y / stars[s].z)) + SCREEN_HEIGHT / 2.0f;
+    using glm::mat3;
+    using glm::vec3;
+    vec3 v0 = triangles[i].v0;
+    vec3 v1 = triangles[i].v1;
+    vec3 v2 = triangles[i].v2;
+    vec3 e1 = v1 - v0;
+    vec3 e2 = v2 - v0;
+    vec3 b = start - v0;
+    mat3 A(-dir, e1, e2);
+    vec3 x = glm::inverse(A) * b;
 
-    // fade the brightness of the stars, i.e. let it depend on the distance to the camera, from instructions
-    vec3 color = 0.2f * vec3(1, 1, 1) / (stars[s].z * stars[s].z);
+    // cramers rule (?)
+    float t = x[0];
+    float u = x[1];
+    float v = x[2];
 
-    sdlAux->putPixel(u, v, color);
+    // equations 7,8,9 & 11, they all have to be true
+    if (u >= 0 && v >= 0 && u + v <= 1 && t >= 0)
+    {
+      vec3 intersection = start + (t * dir);
+      float distance = glm::distance(intersection, start);
+      if (distance < m)
+      {
+        // here, m is updated with the distance. if not done, objects will not be placed in right order.
+        m = distance;
+        closestIntersection.distance = distance;
+        closestIntersection.position = intersection;
+        closestIntersection.triangleIndex = i;
+      }
+      intersect = true;
+    }
   }
-
-  sdlAux->render();
+  return intersect;
 }
 
+/*FROM LAB2*/
+void Rotate()
+{
+  R = mat3(cos(yaw), 0, sin(yaw), 0, 1, 0, -sin(yaw), 0, cos(yaw));
+}
+
+/*FROM LAB2*/
 void Update()
 {
+  // Compute frame time:
   int t2 = SDL_GetTicks();
   float dt = float(t2 - t);
   t = t2;
-  float v = 0.001f;
+  cout << "Render time: " << dt << " ms." << endl;
 
-  for (int s = 0; s < stars.size(); ++s)
+  const Uint8 *state = SDL_GetKeyboardState(NULL);
+  float anglechange = ((2.0f * pi) / 1000.0f) * dt;
+
+  // from instructions: vectors representing the axes directions that we update R for
+  vec3 right(R[0][0], R[0][1], R[0][2]);   // x axis
+  vec3 down(R[1][0], R[1][1], R[1][2]);    // y axis
+  vec3 forward(R[2][0], R[2][1], R[2][2]); // z axis
+
+  // controls camera. i am not sure of the signs for change, + or - gives different directions.
+  if (state[SDL_SCANCODE_UP])
   {
-    stars[s].z = stars[s].z - (v * dt); // equation 1
+    cameraPos += anglechange * forward;
+  }
+  if (state[SDL_SCANCODE_DOWN])
+  {
+    cameraPos -= anglechange * forward;
+  }
+  if (state[SDL_SCANCODE_RIGHT])
+  {
+    cameraPos += anglechange * right;
+  }
+  if (state[SDL_SCANCODE_LEFT])
+  {
+    cameraPos -= anglechange * right;
+  }
+  if (state[SDL_SCANCODE_Z])
+  {
+    yaw += anglechange;
+    // cameraPos += anglechange * right;
+    Rotate();
+  }
+  if (state[SDL_SCANCODE_X])
+  {
+    yaw -= anglechange;
+    // cameraPos += anglechange * right;
+    Rotate();
+  }
 
-    if (stars[s].z <= 0)
-      stars[s].z += 1;
-    if (stars[s].z > 1)
-      stars[s].z -= 1;
+  if (state[SDL_SCANCODE_W])
+  {
+    lightPos += anglechange * forward;
+  }
+
+  if (state[SDL_SCANCODE_S])
+  {
+    lightPos -= anglechange * forward;
+  }
+
+  if (state[SDL_SCANCODE_A])
+  {
+    lightPos += anglechange * right;
+  }
+
+  if (state[SDL_SCANCODE_D])
+  {
+    lightPos -= anglechange * right;
+  }
+
+  if (state[SDL_SCANCODE_Q])
+  {
+    lightPos += anglechange * down;
+  }
+
+  if (state[SDL_SCANCODE_E])
+  {
+    lightPos -= anglechange * down;
   }
 }
 
-void InterpolateFloat(float a, float b, vector<float> &result)
+/* IMPLEMENTATION */
+
+/*THE AMBIENT COMPONENT*/
+vec3 Ambient(const Intersection &i)
 {
-  // when calling Interpolate, the result vector will vary based on number of pixels in x and y directions
-  //(1-t)a + t*b
+  // additive component, does not need arguments. global terms.
+  // ka is between 0 and 1. i choose ambient constant for silver.
+  float ka = 0.19225f;
 
-  // special case: take the average of a and b
+  vec3 ambient = ka * lightColor;
 
-  if (result.size() == 1)
-  {
-    result[0] = (a + b) / 2.0f;
-  }
-  for (int i = 0; i < result.size(); ++i)
-  {
-    // t ∈ [0, 1]
-    float t = i / ((float)result.size() - 1.0f);
-    result[i] = ((1 - t) * a) + b * t;
-  }
+  /*CODE LINE for testing the surface color as light intensity instead of global lightColor variables */
+  // vec3 ambient = ka * triangles[i.triangleIndex].color;
+
+  return ambient;
 };
 
-void Interpolate(vec3 a, vec3 b, vector<vec3> &result)
+/*THE DIFFUSE COMPONENT*/
+vec3 Diffuse(const Intersection &i)
 {
-  if (result.size() == 1)
-  {
-    result[0] = (a + b) / 2.0f;
-  }
+  // needs surface normal and light direction. take argument of Intersection (just like DirectLight, same vectors)
+  vec3 normal = triangles[i.triangleIndex].normal;
+  vec3 lightdirection = lightPos - i.position;
 
-  for (int i = 0; i < result.size(); i++)
+  float cosTheta = glm::dot(glm::normalize(lightdirection), normal);
+
+  // taken directly, and adapted, from lab2 DirectLight
+  Intersection intersection;
+  if (ClosestIntersection(lightPos, -glm::normalize(lightdirection), triangles, intersection))
   {
-    //(1-t)a + t*b, for each of the three pixel values
-    // t ∈ [0, 1]
-    float t = float(i) / (float(result.size()) - 1.0f);
-    result[i].x = (1 - t) * a.x + t * b.x;
-    result[i].y = (1 - t) * a.y + t * b.y;
-    result[i].z = (1 - t) * a.z + t * b.z;
+    if (intersection.distance < glm::distance(lightPos, i.position) - 0.001f)
+    {
+      return vec3(0, 0, 0);
+    }
   }
+  // diffuse surface constant of silver, to make some shininess
+  float kd = 0.50754f;
+
+  /*CODE LINE for testing the surface color as light intensity instead of global lightColor variables */
+  // vec3 diffuse = kd * glm::max(cosTheta * triangles[i.triangleIndex].color, 0.0f);
+
+  vec3 diffuse = kd * glm::max(cosTheta * lightColor, 0.0f);
+  return diffuse;
+};
+
+/*THE SPECULAR COMPONENT*/
+vec3 Specular(const Intersection &i)
+{
+  // reflection direction R and the the direction from the surface point towards the view V. can take argument of Intersection
+  // to get those vectors to work with.
+
+  // view direction from surface to view/camera
+  vec3 viewdirection = cameraPos - i.position;
+
+  // direction from surface to light
+  vec3 lightdirection = lightPos - i.position;
+
+  vec3 normal = triangles[i.triangleIndex].normal;
+
+  // direction of perfect reflection, with respect to noraml
+  vec3 perfect = glm::reflect(-glm::normalize(lightdirection), normal);
+
+  // specular exponent
+  float n = 50;
+
+  float cosTheta = glm::pow(glm::dot(perfect, glm::normalize(viewdirection)), n);
+  // specular surface constant
+  float ks = 0.508273;
+  vec3 specular = ks * lightColor * glm::max(cosTheta, 0.0f);
+
+  return specular;
 };
